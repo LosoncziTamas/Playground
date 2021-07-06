547cc4a3493ff94a4f982d3f2faf2a7af6a4241f
Tamás Losonczi <losonczi.tamas91@gmail.com>
2021-07-06 08:34:15 +0200

    add basic movement code

--- Assets/Scripts/Prototype02/Hero.cs
+++ Assets/Scripts/Prototype02/Hero.cs
@@ -5,17 +5,27 @@ namespace Prototype02
 {
     public class Hero : MonoBehaviour
     {
-        private Animator _animator;
+        public float Speed = 1.0f;
+        
         private static readonly int Grounded = Animator.StringToHash("Grounded");
         private static readonly int AirSpeedY = Animator.StringToHash("AirSpeedY");
+        
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+        private Rigidbody2D _rigidbody2D;
 
         [SerializeField] private CollisionSensor _groundCollisionSensor;
 
         private bool _grounded;
+        private int _facingDirection;
+        private static readonly int AnimState = Animator.StringToHash("AnimState");
 
         private void Awake()
         {
+            _facingDirection = 1;
             _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
         private void FixedUpdate()
@@ -39,19 +49,38 @@ namespace Prototype02
 
         private void Update()
         {
+            var move = false;
             var horizontal = Input.GetAxis("Horizontal");
             if (horizontal < 0)
             {
-                
+                if (_facingDirection > 0)
+                {
+                    _spriteRenderer.flipX = true;
+                }
+                _facingDirection = -1;
+                move = true;
             }
             else if (horizontal > 0)
             {
-                
+                if (_facingDirection < 0)
+                {
+                    _spriteRenderer.flipX = false;
+                }
+                _facingDirection = 1;
+                move = true;
+            }
+
+            if (move)
+            {
+                _animator.SetInteger(AnimState, 1);
+                _rigidbody2D.MovePosition(_rigidbody2D.position + horizontal * Vector2.right * Speed);
             }
             else
             {
-                
+                _animator.SetInteger(AnimState, 0);
             }
+
+            
         }
     }
 }
