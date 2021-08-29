1657685e67cbea1ac88188a4511d74c8000331cc
Tamás Losonczi <losonczi.tamas91@gmail.com>
2021-08-29 15:05:59 +0200

    simplify CameraController.cs, add vertical block tiles

--- Assets/Scripts/Prototype02/CameraController.cs
+++ Assets/Scripts/Prototype02/CameraController.cs
@@ -1,46 +1,26 @@
-using System;
 using UnityEngine;
 
 namespace Prototype02
 {
-    [RequireComponent(typeof(Camera))]
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private GameData _gameData;
         
-        private Camera _camera;
         private HeroController _hero;
         
         private void Start()
         {
-            _camera = GetComponent<Camera>();
             _hero = HeroController.Instance;
         }
-
+        
         private void Update()
         {
-            if (_hero.Moving)
+            var horizontalOffset = _hero.transform.position.x - transform.position.x;
+            var distance = Mathf.Abs(horizontalOffset);
+            if (distance > _gameData.cameraMovementMaxDistance)
             {
-                var distance = Vector2.Distance(_hero.transform.position, transform.position);
-                Debug.Log("Distance " + distance);
-                if (distance > _gameData.cameraMovementMaxDistance)
-                {
-                    var currPos = transform.position;
-                    var target = Vector2.MoveTowards(currPos, _hero.transform.position + _gameData.cameraHeroOffset, _gameData.cameraCatchUpSpeed * Time.deltaTime);
-                    transform.position = new Vector3(target.x, target.y, currPos.z);
-                } 
-                else  if (distance > _gameData.cameraMovementMinDistance)
-                {
-                    var currPos = transform.position;
-                    var target = Vector2.MoveTowards(currPos, _hero.transform.position + _gameData.cameraHeroOffset, _gameData.cameraSpeedDuringMovement * Time.deltaTime);
-                    transform.position = new Vector3(target.x, target.y, currPos.z);
-                }
-            }
-            else
-            {
-                var currPos = transform.position;
-                var target = Vector2.MoveTowards(currPos, _hero.transform.position + _gameData.cameraHeroOffset, _gameData.cameraCatchUpSpeed * Time.deltaTime);
-                transform.position = new Vector3(target.x, target.y, currPos.z);
+                var offset = distance - _gameData.cameraMovementMaxDistance;
+                transform.position += Vector3.right * offset * Mathf.Sign(horizontalOffset);
             }
         }
     }
--- Assets/Scripts/Prototype02/GameData.cs
+++ Assets/Scripts/Prototype02/GameData.cs
@@ -5,10 +5,6 @@ namespace Prototype02
     [CreateAssetMenu(menuName = "My Assets/Game Data")]
     public class GameData : ScriptableObject
     {
-        public float cameraSpeedDuringMovement;
-        public float cameraCatchUpSpeed;
-        public float cameraMovementMinDistance;
         public float cameraMovementMaxDistance;
-        public Vector3 cameraHeroOffset;
     }
 }
