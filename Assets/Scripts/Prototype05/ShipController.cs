4bebc73e18bf35f8fd7794fd1ecdcd6650130de1
Tamás Losonczi <losonczi.tamas91@gmail.com>
2021-09-22 17:30:52 +0200

    control WIP

--- Assets/Scripts/Prototype05/ShipController.cs
+++ Assets/Scripts/Prototype05/ShipController.cs
@@ -1,26 +1,29 @@
+using System;
 using UnityEngine;
 
 namespace Prototype05
 {
     public class ShipController : MonoBehaviour
     {
-
         private Vector3 _velocity;
-        
-        void Start()
+        private Transform _cachedTransform;
+
+        private void Start()
         {
-        
+            _cachedTransform = transform;
         }
 
         private void Update()
         {
             var rotation = -Input.GetAxis("Horizontal");
             var acceleration = Input.GetAxis("Vertical");
-            transform.Rotate(Vector3.forward, rotation);
-            Debug.Log("rotation" + transform.rotation);
-            Debug.Log("eulerAngles" + transform.rotation.eulerAngles);
-            Debug.Log("eulerAngles * acceleration" + transform.rotation.eulerAngles * acceleration);
-            
+            var vec3Acceleration = _cachedTransform.right * acceleration;
+            _cachedTransform.Rotate(Vector3.forward, rotation);
+            var drag = -0.1f;
+            var speed = 5.0f;
+            _velocity.x += Math.Max(vec3Acceleration.x + drag, 0) * speed;
+            _velocity.y += Math.Max(vec3Acceleration.y + drag, 0) * speed;
+            _cachedTransform.position += _velocity;
         }
     }
 }
